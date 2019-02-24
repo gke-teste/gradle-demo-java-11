d4a71aabc05a5e01590cd9d17841be7c2593256a
Renã Souza <rena.souza@keyrus.com.br>
2019-02-24 17:18:51 -0300

    Initial import

---
+++ src/main/java/com/example/gradledemojava11/GradleDemoJava11Application.java
@@ -0,0 +1,13 @@
+package com.example.gradledemojava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemoJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemoJava11Application.class, args);
+	}
+
+}

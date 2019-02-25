0855a435f3d3c602fd8b51dbc669415cfa31245e
Renã Souza <rena.souza@keyrus.com.br>
2019-02-24 21:02:12 -0300

    Adding a field

--- src/main/java/com/example/gradledemojava11/model/Greeting.java
+++ src/main/java/com/example/gradledemojava11/model/Greeting.java
@@ -3,6 +3,15 @@ package com.example.gradledemojava11.model;
 public class Greeting {
 
     private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
